8b3dba03f839078128f1a1e858b1ce63568881b8
Muhammad Ridho <73427989+shiftroll@users.noreply.github.com>
2025-05-05 01:33:46 +0700

    commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import FinancialCalculator from '@/components/FinancialCalculator';
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#31322C] p-3 overflow-hidden">
-      <div className="w-full max-w-4xl">
+      <div className="w-full max-w-4xl h-full">
         <FinancialCalculator />
       </div>
     </div>
